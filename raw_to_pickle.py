184386cd5cb95ad2d802aa13d905d6bce18e05ad
klankey <mail@mecheng.f9.co.uk>
2024-11-06 13:54:21 +0000

    Update raw_to_pickle.py

--- raw_to_pickle.py
+++ raw_to_pickle.py
@@ -4,7 +4,9 @@ root_dir = "Datasets/MindBigData - The Visual MNIST of Brain Digits/2022Data"
 
 input_file = "train_MindBigDataVisualMnist2021-Muse2v0.17.csv"
 output_file = "train_MindBigDataVisualMnist2021-Muse2v0.17.pkl"
-
+## TEST
+# input_file = "test_MindBigDataVisualMnist2021-Muse2v0.17.csv"
+# output_file = "test_MindBigDataVisualMnist2021-Muse2v0.17.pkl"
 ## Takes the large raw data file given from MBD and creates a pandas datatable for easy usage, savig the DF to pickle file.
 
 big_df = batch.batch_process_csv_pandas(f"{root_dir}/{input_file}",f"{root_dir}/{output_file}")
